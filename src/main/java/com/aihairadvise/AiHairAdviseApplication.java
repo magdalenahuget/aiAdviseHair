76465e2c05c3bea2cb7947b7c25cd17ab440307b
magdalenahuget <magdalena.huget@gmail.com>
2024-02-25 11:40:32 +0100

    Initial commit

---
+++ src/main/java/com/aihairadvise/AiHairAdviseApplication.java
@@ -0,0 +1,13 @@
+package com.aihairadvise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHairAdviseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiHairAdviseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aihairadvise/AiHairAdviseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aihairadvise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHairAdviseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
