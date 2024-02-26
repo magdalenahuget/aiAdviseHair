68fd5a0d323cf72538f184fb6a35d901f34a6b9e
magdalenahuget <magdalena.huget@gmail.com>
2024-02-26 17:42:56 +0100

    Fix imports

--- src/main/java/com/aihairadvise/repository/AdviceRepository.java
+++ src/main/java/com/aihairadvise/repository/AdviceRepository.java
@@ -1,5 +1,7 @@
 package com.aihairadvise.repository;
 
+import com.aihairadvise.model.Advice;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
