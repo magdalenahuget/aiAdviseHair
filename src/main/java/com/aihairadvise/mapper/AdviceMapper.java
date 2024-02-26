feb710b69d26d357abf641ac0de5a8258a79323b
magdalenahuget <magdalena.huget@gmail.com>
2024-02-26 18:01:29 +0100

    Add advice mapper

--- src/main/java/com/aihairadvise/mapper/AdviceMapper.java
+++ src/main/java/com/aihairadvise/mapper/AdviceMapper.java
@@ -1,4 +1,21 @@
 package com.aihairadvise.mapper;
 
+import com.aihairadvise.dto.request.AdviceResponseDto;
+import com.aihairadvise.model.Advice;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AdviceMapper {
+
+    public AdviceResponseDto toResponseDTO(Advice advice) {
+        AdviceResponseDto responseDTO = new AdviceResponseDto();
+        responseDTO.setId(advice.getId());
+        responseDTO.setFaceShape(advice.getFaceShape().toString());
+        responseDTO.setForeheadHeight(advice.getForeheadHeight().toString());
+        responseDTO.setNoseSize(advice.getNoseSize().toString());
+        responseDTO.setLipFullness(advice.getLipFullness().toString());
+        responseDTO.setEyeColor(advice.getEyeColor().toString());
+        responseDTO.setGender(advice.getGender().toString());
+        return responseDTO;
+    }
 }
