10cad3ad0f85c1c0c310f698aaafe11d5f4fcdde
magdalenahuget <magdalena.huget@gmail.com>
2024-02-26 17:34:32 +0100

    Add fields and persistance annotations

--- src/main/java/com/aihairadvise/model/Advice.java
+++ src/main/java/com/aihairadvise/model/Advice.java
@@ -1,4 +1,41 @@
 package com.aihairadvise.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "advice")
 public class Advice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "face_shape")
+    private FaceShape faceShape;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "forehead_height")
+    private ForeheadHeight foreheadHeight;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "nose_size")
+    private NoseSize noseSize;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "lip_fullness")
+    private LipFullness lipFullness;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "eye_color")
+    private EyeColor eyeColor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+
+
 }
