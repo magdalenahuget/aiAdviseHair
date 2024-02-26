d17c440b9272134ac0b2dcf09593af5336b3f320
magdalenahuget <magdalena.huget@gmail.com>
2024-02-26 17:57:55 +0100

    Add fields to dtos

--- src/main/java/com/aihairadvise/dto/request/AdviceRequestDto.java
+++ src/main/java/com/aihairadvise/dto/request/AdviceRequestDto.java
@@ -1,4 +1,15 @@
 package com.aihairadvise.dto.request;
 
-public class GetAdviceRequestDto {
+import com.aihairadvise.model.Advice;
+
+public class AdviceRequestDto {
+
+    private Advice.FaceShape faceShape;
+    private Advice.ForeheadHeight foreheadHeight;
+    private Advice.NoseSize noseSize;
+    private Advice.LipFullness lipFullness;
+    private Advice.EyeColor eyeColor;
+    private Advice.Gender gender;
+
+    // Constructors, getters, and setters
 }
--- src/main/java/com/aihairadvise/dto/request/AdviceResponseDto.java
+++ src/main/java/com/aihairadvise/dto/request/AdviceResponseDto.java
@@ -1,4 +1,13 @@
 package com.aihairadvise.dto.request;
 
-public class GetAdviceResponseDto {
+public class AdviceResponseDto {
+
+    private Long id;
+    private String faceShape;
+    private String foreheadHeight;
+    private String noseSize;
+    private String lipFullness;
+    private String eyeColor;
+    private String gender;
+
 }
