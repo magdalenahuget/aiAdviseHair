74d44d43e2a7153b1bc7959734a84c0a55a1f53e
magdalenahuget <magdalena.huget@gmail.com>
2024-02-26 18:13:15 +0100

    Fix mapper integration

--- src/main/java/com/aihairadvise/service/AdviceService.java
+++ src/main/java/com/aihairadvise/service/AdviceService.java
@@ -3,6 +3,7 @@ package com.aihairadvise.service;
 import com.aihairadvise.configuration.exception.AdviceNotFoundException;
 import com.aihairadvise.dto.request.AdviceRequestDto;
 import com.aihairadvise.dto.request.AdviceResponseDto;
+import com.aihairadvise.mapper.AdviceMapper;
 import com.aihairadvise.model.Advice;
 import com.aihairadvise.repository.AdviceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +15,12 @@ import java.util.Optional;
 public class AdviceService {
 
     private final AdviceRepository adviceRepository;
+    private final AdviceMapper adviceMapper;
 
     @Autowired
-    public AdviceService(AdviceRepository adviceRepository) {
+    public AdviceService(AdviceRepository adviceRepository, AdviceMapper adviceMapper) {
         this.adviceRepository = adviceRepository;
+        this.adviceMapper = adviceMapper;
     }
 
     public AdviceResponseDto getAdviceByAttributes(AdviceRequestDto requestDTO) {
@@ -30,8 +33,7 @@ public class AdviceService {
                 requestDTO.getGender()
         );
         if (adviceOptional.isPresent()) {
-            // mapper
-            return null;
+            return adviceMapper.toResponseDTO(adviceOptional.get());
         } else {
             throw new AdviceNotFoundException("Advice not found for given attributes");
         }
