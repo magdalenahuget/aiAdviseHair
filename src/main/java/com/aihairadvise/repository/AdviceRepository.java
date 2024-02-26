b8ddc898081b819930565cd4521332ec02f17d9d
magdalenahuget <magdalena.huget@gmail.com>
2024-02-26 17:42:32 +0100

    Add repository spring annotation and jpaRepository inheritance

--- src/main/java/com/aihairadvise/repository/AdviceRepository.java
+++ src/main/java/com/aihairadvise/repository/AdviceRepository.java
@@ -1,4 +1,7 @@
 package com.aihairadvise.repository;
 
-public class AdviceRepository {
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdviceRepository extends JpaRepository<Advice, Long> {
 }
