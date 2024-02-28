cfb3a7c17890bea4b522bb376fea2ab2c77c9299
magdalenahuget <magdalena.huget@gmail.com>
2024-02-28 18:19:03 +0100

    Add advices generator and display in terminal

--- src/main/java/com/aihairadvise/utils/generators/AdvicesGenerator.java
+++ src/main/java/com/aihairadvise/utils/generators/AdvicesGenerator.java
@@ -1,5 +1,41 @@
 package com.aihairadvise.utils.generators;
 
-public class AdvicesGenerator {
+import com.aihairadvise.model.Advice;
 
+public class AdvicesGenerator {
+    public static void main(String[] args) {
+        Advice.FaceShape[] faceShapes = Advice.FaceShape.values();
+        Advice.ForeheadHeight[] foreheadHeights = Advice.ForeheadHeight.values();
+        Advice.NoseSize[] noseSizes = Advice.NoseSize.values();
+        Advice.LipFullness[] lipFullnesses = Advice.LipFullness.values();
+        Advice.EyeColor[] eyeColors = Advice.EyeColor.values();
+        Advice.Gender[] genders = Advice.Gender.values();
+
+        int counter = 0;
+        for (Advice.FaceShape faceShape : faceShapes) {
+            for (Advice.ForeheadHeight foreheadHeight : foreheadHeights) {
+                for (Advice.NoseSize noseSize : noseSizes) {
+                    for (Advice.LipFullness lipFullness : lipFullnesses) {
+                        for (Advice.EyeColor eyeColor : eyeColors) {
+                            for (Advice.Gender gender : genders) {
+                                counter++;
+                                Advice advice = new Advice();
+                                advice.setFaceShape(faceShape);
+                                advice.setForeheadHeight(foreheadHeight);
+                                advice.setNoseSize(noseSize);
+                                advice.setLipFullness(lipFullness);
+                                advice.setEyeColor(eyeColor);
+                                advice.setGender(gender);
+                                // Tutaj możesz ustawić pole recommendation na dowolną wartość lub pominąć ten krok
+                                advice.setRecommendation("Twoja rekomendacja");
+
+                                // Wyświetlanie stworzonego obiektu
+                                System.out.println(counter + ". " + advice.toString());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
