397581e14a5db9cf1c2058f20de2416a0745c8c1
magdalenahuget <magdalena.huget@gmail.com>
2024-03-03 21:01:16 +0100

    Refactor by display method extractiom

--- src/main/java/com/aihairadvise/utils/generators/AdvicesGenerator.java
+++ src/main/java/com/aihairadvise/utils/generators/AdvicesGenerator.java
@@ -27,25 +27,28 @@ public class AdvicesGenerator {
                                 advice.setEyeColor(eyeColor);
                                 advice.setGender(gender);
                                 advice.setRecommendation("Example recommendation");
-//                                System.out.println(counter + ". " + advice.toString());
-                                System.out.println(counter
-                                        + ". Give your most recommended hairstyle based on the following human characteristics: "
-                                        + advice.getFaceShape() + " face shape, "
-                                        + advice.getForeheadHeight() + " forehead height, "
-                                        + advice.getNoseSize() + " nose size, "
-                                        + advice.getLipFullness() + " lipfullness, "
-                                        + advice.getEyeColor() + " eye color, "
-                                        + advice.getGender() + " gender."
-                                        + " The description should be in 10 sentences and maximum of 150 words " +
-                                        "without mentioning consulting with a specialist. " +
-                                        "Make sure to have at least summarizing 2 sentences. " +
-                                        "The last part of your answer should be an summarization and start that with word \"Overall\". " +
-                                        "Do not use break line between 'overall' section and description.");
+                                displayHairSpecificPrompt(counter, advice);
                             }
                         }
                     }
                 }
             }
         }
+    }
+
+    private static void displayHairSpecificPrompt(int counter, Advice advice) {
+        System.out.println(counter
+                + ". Give your most recommended hairstyle based on the following human characteristics: "
+                + advice.getFaceShape() + " face shape, "
+                + advice.getForeheadHeight() + " forehead height, "
+                + advice.getNoseSize() + " nose size, "
+                + advice.getLipFullness() + " lipfullness, "
+                + advice.getEyeColor() + " eye color, "
+                + advice.getGender() + " gender."
+                + " The description should be in 10 sentences and maximum of 150 words " +
+                "without mentioning consulting with a specialist. " +
+                "Make sure to have at least summarizing 2 sentences. " +
+                "The last part of your answer should be an summarization and start that with word \"Overall\". " +
+                "Do not use break line between 'overall' section and description.");
     }
 }
