468decda694abe91eb4caa38108ad59ff36cf67c
magdalenahuget <magdalena.huget@gmail.com>
2024-03-07 23:13:33 +0100

    Add display recommendations

--- src/main/java/com/aihairadvise/utils/extractors/RecommendationExtractor.java
+++ src/main/java/com/aihairadvise/utils/extractors/RecommendationExtractor.java
@@ -1,9 +1,6 @@
 package com.aihairadvise.utils.extractors;
 
-import com.sun.tools.javac.Main;
-
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class RecommendationExtractor {
@@ -11,12 +8,13 @@ public class RecommendationExtractor {
     public static void main(String[] args) {
         try {
             String[] descriptions = extractAndSplitDescriptions("src/main/resources/aiAnswersSecondPart.txt");
-//            printArray(descriptions);
-            System.out.println(Arrays.toString(descriptions));
             System.out.println(">>>>>>>>>>>>>>");
-            System.out.println(descriptions[2]);
             System.out.println(">>>>>>>>>>>>>>");
 
+            for (String description : descriptions) {
+                String textAfterChatGPT = extractTextAfterChatGPT(description);
+            }
+            System.out.println(">>>>>>>>>>>>>>");
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,5 +43,16 @@ public class RecommendationExtractor {
             System.out.println("Give your most" + s);
             System.out.println("=============");
         }
+    }
+
+    public static String extractTextAfterChatGPT(String recommendation) {
+        String[] recommendationSplit = recommendation.split("ChatGPT");
+        if (recommendationSplit.length > 1) {
+            // REMEMBER! First number is not printed. It starts with 180, but it isn't displayed.
+            System.out.println(recommendationSplit[1]);
+            return recommendationSplit[1];
+
+        }
+        return "empty";
     }
 }
