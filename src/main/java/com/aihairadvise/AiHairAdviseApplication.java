49e4bf09c66d40fe26189bcab70a0d0fe381ccac
magdalenahuget <magdalena.huget@gmail.com>
2024-03-16 23:21:02 +0100

    Add generating base data code

--- src/main/java/com/aihairadvise/AiHairAdviseApplication.java
+++ src/main/java/com/aihairadvise/AiHairAdviseApplication.java
@@ -4,15 +4,20 @@ import com.aihairadvise.service.AdvicesGeneratorService;
 import com.aihairadvise.service.AdvicesGeneratorServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AiHairAdviseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AiHairAdviseApplication.class, args);
-
-		AdvicesGeneratorService generatorService = new AdvicesGeneratorServiceImpl();
-		generatorService.generateAndDisplayAdvices();
 	}
 
+	// Needed to generate base data
+//	public static void main(String[] args) {
+//		ConfigurableApplicationContext context = SpringApplication.run(AiHairAdviseApplication.class, args);
+//
+//		AdvicesGeneratorService generatorService = context.getBean(AdvicesGeneratorService.class);
+//		generatorService.generateAndSaveAdvices();
+//	}
 }
