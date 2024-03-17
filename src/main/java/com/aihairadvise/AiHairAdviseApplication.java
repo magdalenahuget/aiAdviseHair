45651d419d162ec0a86942b57ab14f30582d5660
magdalenahuget <magdalena.huget@gmail.com>
2024-03-17 11:32:46 +0100

    Fix spring app component scan

--- src/main/java/com/aihairadvise/AiHairAdviseApplication.java
+++ src/main/java/com/aihairadvise/AiHairAdviseApplication.java
@@ -7,8 +7,9 @@ import com.aihairadvise.service.RecommendationExtractorServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.aihairadvise"})
 public class AiHairAdviseApplication {
 
 	public static void main(String[] args) {
