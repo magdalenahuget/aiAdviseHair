0d206ef89a4490c8ea2199b0c64200ffc11c900d
magdalenahuget <magdalena.huget@gmail.com>
2024-03-17 01:28:22 +0100

    Add service and mapper for get advice by id

--- src/main/java/com/aihairadvise/service/AdviceService.java
+++ src/main/java/com/aihairadvise/service/AdviceService.java
@@ -2,9 +2,11 @@ package com.aihairadvise.service;
 
 import com.aihairadvise.dto.request.AdviceRequestDto;
 import com.aihairadvise.dto.response.AdviceResponseDto;
+import com.aihairadvise.dto.response.GetAdviceResponseDto;
 import com.aihairadvise.model.Advice;
 
 public interface AdviceService {
     AdviceResponseDto getAdviceByAttributes(AdviceRequestDto requestDTO);
     Advice updateRecommendation(Long id, String recommendation);
+    GetAdviceResponseDto getAdviceById(Long adviceId);
 }
